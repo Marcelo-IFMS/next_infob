4ea6add94050551aeb40eff2b34ed55e6e0a8acb
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-05-16 12:26:16 +0000

    add: useEffect v2

--- src/pages/useeffect.js
+++ src/pages/useeffect.js
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Useeffect() {
     const [numero, setNumero] = useState(0);
+    setTimeout(() => {
+        setNumero(numero + 1);
+    },3000)
+
+    useEffect(() => {
+        document.title = `UseEffect ${numero}`;
+    },[numero])
+
     return <>
         <div className="text-center">
             <h1> useEffect</h1>
-            <h2>{numero === 0 ? "imagem" : numero}</h2>
+            <h2>{numero === 0 ? <img src={"./loading-2.gif"} height={"50px"} width={"100px"}/> : numero}</h2>
         </div>
     </>
 }
