5d7f020325d5cb88db86a0beb20a45b87ef2fc80
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-05-16 12:04:56 +0000

    add:useeffect

--- src/pages/useeffect.js
+++ src/pages/useeffect.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default function Useeffect() {
+    const [numero, setNumero] = useState(0);
     return <>
         <div className="text-center">
             <h1> useEffect</h1>
+            <h2>{numero === 0 ? "imagem" : numero}</h2>
         </div>
     </>
 }
