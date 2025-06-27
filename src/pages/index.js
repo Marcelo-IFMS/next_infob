05ec9689fc49005b7c53ead0d9ad75f166d147a7
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-06-27 11:54:47 +0000

    ADD: index notícias

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
-
-export default function Home() {
+import { Container, Row } from "react-bootstrap";
+import Cards from "./components/cards";
+export async function getServerSideProps() {
+  // Fetch data from  API notícias
+  const res = await fetch('http://localhost:3000/api/noticias/apinoticias')
+  const repo = await res.json()
+  // Pass data to the page via props
+  return { props: { noticias: repo } }
+}
+export default function Home({ noticias }) {
+  console.log("noticias", noticias);
   return <>
-    <h1>
-      {4+2}
-      <p>Bem Vindos</p>
-
-    </h1>
+    <Container>
+      <Row xs={1} md={3}>
+        {Array.isArray(noticias) ?
+          noticias.map(noticia => (
+            <Cards idnoticia={noticia.idnoticia}
+              conteudonoticia={noticia.conteudonoticia}
+              titulonoticia={noticia.titulonoticia}
+              datahoracadastro={noticia.datahoracadastro}
+              tiponoticia={noticia.tiponoticia} />
+          ))
+          : "não"}
+      </Row>
+    </Container>
   </>
 }
