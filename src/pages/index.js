07f9c981d60076c5d45a3c7692b5a9532619769a
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-07-10 11:39:18 -0400

    alterado Main

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ export default function Home({ noticias }) {
   return <>
     <Container>
       <Row xs={1} md={3}>
+        Main
         {Array.isArray(noticias) ?
           noticias.map(noticia => (
             <Cards idnoticia={noticia.idnoticia}
